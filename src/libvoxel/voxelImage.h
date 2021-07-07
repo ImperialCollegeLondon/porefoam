1cf04e629570721776d2cd4f59847b0075320e3c
Ali Raeini <a.q.raeini@imperial.ac.uk>
2021-07-07 20:35:11 +0100

    undo remove of the constructor that broke many other codes!

--- src/libvoxel/voxelImage.h
+++ src/libvoxel/voxelImage.h
@@ -175,8 +175,8 @@ class voxelImageT: public voxelImageTBase, public voxelField<T>   //!  3D image 
 
 	voxelImageT():X0_(0.,0.,0.),dx_(1,1,1) {};
 
-	//voxelImageT(int n1, int n2, int n3, T value)
-	//: voxelField<T>( n1,  n2,  n3,  value),  X0_(0.,0.,0.), dx_(1,1,1) {}
+	voxelImageT(int n1, int n2, int n3, T value) //do not remove, the following constructor will be missused!
+	: voxelField<T>( n1,  n2,  n3,  value),  X0_(0.,0.,0.), dx_(1.,1.,1.) {}
 
 
 	voxelImageT(int3 n, dbl3 dx, dbl3 xmin, T value)
