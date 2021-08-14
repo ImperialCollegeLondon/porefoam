a7547727e2389b551c53d4d7eb28bae8276eed8c
Ali Qaseminejad Raeini <a.q.raeini@imperial.ac.uk>
2021-08-14 21:57:53 +0100

    add tutorials

---
+++ src/porefoam2f/tutorials/READEME.mhd
@@ -0,0 +1,30 @@
+# tutorials
+
+The bash scripts [run_cfmesh.sh](run_cfmesh.sh)  or 
+[run_vxlmesh.sh](run_vxlmesh.sh)  runs sample simulations on a ketton 
+sample, which is downloaded from digital rock portal. The This is to 
+tests the two available mesh grneration options and the culprits of 
+each approach.   You need to copy them and run them from outside the 
+src source code folder.
+
+[run_cfmesh.sh](run_cfmesh.sh) uses cfMesh to generate cell layers near 
+solid walls provided that the mesh is at least a factor of two finer 
+than the roughnesses of the image.   Otherwise cfMesh can crash. The 
+generated mesh has a high non-orthogonality but the two-phase flow 
+solver may be able handle that (further testing needed).
+
+
+[run_vxlmesh.sh](run_vxlmesh.sh) which uses The voxelToFoam(Par) 
+togather with smoothMesh can in theory produce do a similar job as 
+snappyHexMesh (both unable to produce cell layers). Note that using 
+smoothMesh does not seem to improve the quality of simulations, but 
+this needs further testing. You can make thvoxelToFoam and 
+snappyHexMesh approach do not have the limitations of cfMesh but can 
+lead to imperfections with modelling contact line motion. 
+
+Alternatively you can edit the AllRunImageTwoPhase script invoked by 
+run_vxlmesh.sh and uncomment the section which uses snappyHexMesh. This 
+requires installation of a full openfoam version and of course further 
+testing.  Another area which needs further testing is the smoothMesh utility 
+whose mesh smoothing parameters are not roboust and need adjustement in 
+a case by case basis.
---
+++ src/porefoam2f/tutorials/run_cfmesh.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# check
+[[ `pwd` != *"src/"* ]] || ! echo "Please run me from outside src tree" && exit 1
+which AllRunImageTwoPhaseCFMesh || source porefoam/src/script/bashrc
+which AllRunImageTwoPhaseCFMesh || ! echo -e 'Please first `source .../src/script/bashrc`' && exit 1
+
+# download the input image
+if ! [ -e ketton.raw ]; then echo "Downloading ketton image"
+ wget -O ketton.raw https://www.digitalrocksportal.org/projects/125/images/101255/download/ 
+fi
+
+
+
+#keep the unit as micrometers otherwise the AllRunImagePar needs to be edited
+# Create ketton.mhd header 
+cat  <<MHD > ketton.mhd
+ObjectType =  Image
+NDims =       3
+ElementType = MET_UCHAR
+ElementByteOrderMSB = False
+ElementNumberOfChannels = 1
+CompressedData = False
+HeaderSize = 0
+DimSize     = 365  255  225
+ElementSize = 1e-6 1e-6 1e-6 
+Offset      = 0    0    0
+ElementDataFile  = ketton.raw
+
+pore  1 2   // replace 0-1 with 0 and the rest with 1
+
+resample 2 //< make image coarser for quicker testing of mesh generation
+resample 2 //<  compensated for by RefineLevel=2
+
+FaceMedian06   10
+PointMedian032 15  20
+FaceMedian06   10
+PointMedian032 15  20
+
+keepLargest0
+
+// TODO: further smooth the inlet area
+//Notes:
+//	image name:    Ketton_rock_trapped_oil_Segmented_SSb
+//	download link: https://doi.org/10.17612/P7D95F
+MHD
+
+
+export meshTag="Cfm" # set tag to avoid conflict with AllRunImageTwoPhaseCFMesh
+echo "Echo mesh smoothing may degrade the mesh quality"
+
+
+export RefineLevel=2 # test 1.5...
+
+# Clean up in case
+rm -rf ketton$meshTag*/ AllRunImageTwoPhaseCFMesh
+
+# AllRunImageTwoPhaseCFMesh copies itself to the current directory for edit and run
+AllRunImageTwoPhaseCFMesh 
+
+# generate mesh, using cfMesh 
+./AllRunImageTwoPhaseCFMesh
+
+
+./AllRunImageTwoPhaseCFMesh
+./AllRunImageTwoPhaseCFMesh
